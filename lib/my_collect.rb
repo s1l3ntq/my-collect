6b1996a1198c3dda79c125e6162638e31c49cb8e
s1l3ntq <qimiller89@gmail.com>
2020-11-17 13:13:52 -0600

    commit tree 2757c502813aa63783e8962b6c5dcab60c6c408b

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,34 @@
+require 'pry'
+
+def my_collect(languages)
+    q = []
+    m = 0
+    while m < languages.length
+        q << yield(languages[m])
+        m += 1
+    end
+    q
+end
+# def my_collect(language)
+#     languages do |language|
+#         language.upcase
+#     end
+# end
 
 
+# def my_collect(students)
+#     students do |name|
+#     name.split(" ").first
+#     end
+# end
+
+
+
+
+
+
+
+#empty_array = []
+ #   counter = 0
+  #    my_collect(empty_array) do |x|
+   #     counter += 1
